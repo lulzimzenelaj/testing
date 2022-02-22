b0fcdf112a5fffdcd4059dcfc54896b9e9cdfac5
Lulzim Zenelaj <lz47429@ubt-uni.net>
2022-02-22 17:07:25 +0100

    DASDSA

--- express/api/middlewares/checkIfAdmin.js
+++ express/api/middlewares/checkIfAdmin.js
@@ -4,24 +4,14 @@ const checkIfAdmin=async(req,res,next)=>{
     //kjo authToken osht = req.AuthToken
     const {authToken}=req;
     try{
-<<<<<<< HEAD
 
 
-        //czxczx
-
-
-=======
->>>>>>> a7140a34991310c7d79b149a6395360a28ed7bbe
     const user = await admin.auth().verifyIdToken(authToken);
     }catch(err){
         return res.status(401).json({message:"Cannot verify right now"})
     }
     if(user.admin){
-<<<<<<< HEAD
         req.authId=null;
-=======
-        req.authId=user.uid;
->>>>>>> a7140a34991310c7d79b149a6395360a28ed7bbe
     }
     else{
         return res.status(401).json({message:"Not an admin"})
